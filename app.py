12c31c8ab0f20bbdcb0820997a38bf788995a215
Jackson Sommerich <Jackson-S@users.noreply.github.com>
2019-04-07 12:57:10 +1000

    Add exact time to main page.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect
-from datetime import datetime
+from datetime import datetime, timezone
+import json
 import sqlite3
 
 app = Flask(__name__)
@@ -39,15 +40,8 @@ def get_last_feed():
     return last_time[0]
 
 
-@app.route("/get_last_feed_string", methods = ["GET"])
-def get_last_feed_string():
-    db = sqlite3.connect(database_name)
-    last_time_query = db.execute("""SELECT datetime(Time, 'localtime')
-                                    FROM FeedTimes 
-                                    ORDER BY Time DESC""")
-    last_time = last_time_query.fetchone()[0]
-    last_datetime = datetime.fromisoformat(last_time)
-    
+def generate_title_text(last_feed_time):
+    last_datetime = datetime.fromisoformat(last_feed_time)
     # Convert the time difference from seconds to hours
     time_diff = (datetime.now() - last_datetime).total_seconds() / 3600
     if time_diff < 1:
@@ -56,6 +50,33 @@ def get_last_feed_string():
         return "The cat was fed 1 hour ago."
     else:
         return "The cat was fed {} hours ago.".format(int(round(time_diff)))
+
+
+def generate_subtitle_text(last_feed_time):
+    last_datetime = datetime.fromisoformat(last_feed_time)
+    last_datetime_local = last_datetime.replace(tzinfo=timezone.utc)
+    last_datetime_local = last_datetime_local.astimezone(tz=None)
+    current_local_time = datetime.now()
+    print(last_datetime_local.day)
+    if last_datetime_local.day == current_local_time.day:
+        return "Today at {}".format(
+            last_datetime_local.strftime("%I:%M %p"))
+    elif current_local_time.day - 1 == last_datetime_local.day:
+        return "Yesterday at {}".format(
+            last_datetime_local.strftime("%I:%M %p"))
+    else:
+        return "{} at {}".format(
+            last_datetime_local.strftime("%x"), 
+            last_datetime_local.strftime("%H:%M"))
+
+
+@app.route("/get_last_feed_string", methods = ["GET"])
+def get_last_feed_string():
+    last_time = get_last_feed()
+    title_text = generate_title_text(last_time)
+    subtitle_text = generate_subtitle_text(last_time)
+    return_objects = {"title": title_text, "subtitle": subtitle_text}
+    return json.dumps(return_objects)
 
 
 @app.route("/")
--- static/reload.js
+++ static/reload.js
@@ -2,8 +2,10 @@ function update_feed_status() {
     var xhr = new XMLHttpRequest();
     xhr.open('get', '/get_last_feed_string', true);
     xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) { 
-            document.getElementById("feed_status").innerHTML = xhr.responseText;
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var json_data = JSON.parse(xhr.responseText)
+            document.getElementById("feed_status").innerHTML = json_data["title"];
+            document.getElementById("feed_time").innerHTML = json_data["subtitle"];
         }
     }
     xhr.send();
