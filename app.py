85d46ee2a9060ffdeca078a2bdf12b09de439cfe
Jackson Sommerich <Jackson-S@users.noreply.github.com>
2019-04-07 14:57:23 +1000

    Conform to code style

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ app = Flask(__name__)
 
 query.init_database()
 
-@app.route("/record_feed", methods = ["POST"])
+
+@app.route("/record_feed", methods=["POST"])
 def record_feed():
     query.add_feed_to_db()
     return redirect("/")
 
 
-@app.route("/get_last_feed", methods = ["GET"])
+@app.route("/get_last_feed", methods=["GET"])
 def get_last_feed():
     query.add_check_to_db()
     last_feed = query.get_feed_from_db()[0]
@@ -22,7 +23,7 @@ def get_last_feed():
     return last_feed_json
 
 
-@app.route("/get_feed_history", methods = ["GET"])
+@app.route("/get_feed_history", methods=["GET"])
 def get_feed_history():
     query.add_check_to_db()
     feed_history = query.get_feed_from_db()
@@ -30,11 +31,23 @@ def get_feed_history():
     return feed_history_json
 
 
-@app.route("/get_check_history", methods = ["GET"])
+@app.route("/get_check_history", methods=["GET"])
 def get_check_history():
     check_history = query.get_check_from_db()
     check_history_json = json.dumps(check_history)
     return check_history_json
+
+
+@app.route("/get_last_feed_string", methods=["GET"])
+def get_last_feed_string():
+    title_text = generate_title_text()
+    subtitle_text = generate_subtitle_text()
+    return json.dumps({"title": title_text, "subtitle": subtitle_text})
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
 
 
 def generate_title_text():
@@ -44,7 +57,7 @@ def generate_title_text():
     current_time = datetime.now()
     time_difference = current_time - last_feed_time
     time_difference_hours = time_difference.total_seconds() / 3600
-    
+
     if time_difference_hours < 1:
         return "The cat was just fed."
     elif time_difference_hours < 2:
@@ -58,26 +71,15 @@ def generate_subtitle_text():
     last_feed = query.get_last_feed_from_db()[0]
     last_feed_time = datetime.fromisoformat(last_feed)
     current_time = datetime.now()
-    
+
     # Selects from two strings, if not in range then prints alternative
     days = ["Today", "Yesterday"]
     day_index = abs(last_feed_time.day - current_time.day)
     if day_index < len(days):
-        return "{} at {}".format(days[day_index], last_feed_time.strftime("%I:%M %p"))
+        time_string = last_feed_time.strftime("%I:%M %p")
+        return "{} at {}".format(days[day_index], time_string)
     else:
         return last_feed_time.strftime("%a %x at %I:%M %p")
-
-
-@app.route("/get_last_feed_string", methods = ["GET"])
-def get_last_feed_string():
-    title_text = generate_title_text()
-    subtitle_text = generate_subtitle_text()
-    return json.dumps({"title": title_text, "subtitle": subtitle_text})
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
 
 
 if __name__ == "__main__":
--- query.py
+++ query.py
@@ -2,6 +2,7 @@ import sqlite3
 
 database_name = "./cat_feed_times.sql"
 
+
 def init_database():
     db = sqlite3.connect(database_name)
     db.execute("CREATE TABLE IF NOT EXISTS FeedTimes (Time INTEGER)")
@@ -26,7 +27,9 @@ def add_check_to_db():
 
 def get_feed_from_db():
     db = sqlite3.connect(database_name)
-    query = "SELECT datetime(Time, 'localtime') FROM FeedTimes ORDER BY Time DESC"
+    query = """SELECT datetime(Time, 'localtime')
+                FROM FeedTimes
+                ORDER BY Time DESC"""
     previous_feeds = db.execute(query).fetchall()
     db.commit()
     db.close()
@@ -35,7 +38,10 @@ def get_feed_from_db():
 
 def get_last_feed_from_db():
     db = sqlite3.connect(database_name)
-    query = "SELECT datetime(Time, 'localtime') FROM FeedTimes ORDER BY Time DESC LIMIT 1"
+    query = """SELECT datetime(Time, 'localtime')
+                FROM FeedTimes
+                ORDER BY Time DESC
+                LIMIT 1"""
     previous_feed = db.execute(query).fetchone()
     db.commit()
     db.close()
@@ -44,7 +50,9 @@ def get_last_feed_from_db():
 
 def get_check_from_db():
     db = sqlite3.connect(database_name)
-    query = "SELECT datetime(Time, 'localtime') FROM CheckTimes ORDER BY Time DESC"
+    query = """SELECT datetime(Time, 'localtime')
+                FROM CheckTimes
+                ORDER BY Time DESC"""
     times = db.execute(query).fetchall()
     db.commit()
     db.close()
