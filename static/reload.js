a01d2210292550c09d7bf014ecb7fe35a88029d3
Jackson Sommerich <Jackson-S@users.noreply.github.com>
2019-04-07 14:48:06 +1000

    Move to Fetch API

--- static/reload.js
+++ static/reload.js
@@ -1,12 +1,26 @@
-function update_feed_status() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', '/get_last_feed_string', true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var json_data = JSON.parse(xhr.responseText)
-            document.getElementById("feed_status").innerHTML = json_data["title"];
-            document.getElementById("feed_time").innerHTML = json_data["subtitle"];
-        }
-    }
-    xhr.send();
+function feed_status_error(response) {
+    var error = "Unable to get last feed time."
+    document.getElementById("feed_status").innerHTML = error;
+    document.getElementById("feed_time").innerHTML = "Sorry";
+    console.log(error + " " + response);
 }
+
+function update_feed_status() {
+    fetch('/get_last_feed_string').then(
+        function(response) {
+            if (response.status !== 200) {
+                feed_status_error(response.status)
+                return;
+            }
+            response.json().then(
+                function(data) {
+                    document.getElementById("feed_status").innerHTML = data["title"];
+                    document.getElementById("feed_time").innerHTML = data["subtitle"];
+                }
+            );
+        }
+    ).catch(function(err) {feed_status_error(err)});
+}
+
+// Update once per minute
+setInterval(update_feed_status, 1000 * 60);
